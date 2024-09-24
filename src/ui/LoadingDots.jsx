cb21883e037e1bfd476fb316736049501bedef45
Jonah <Jonahemmanuel54@gmail.com>
2024-09-24 08:18:04 +0100

    feat: implement reusable buttons and form inputs

---
+++ src/ui/LoadingDots.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const LoadingDots = () => {
+    return(
+        <div className="loading-dots">
+            <div className="loading-dot"></div>
+            <div className="loading-dot"></div>
+            <div className="loading-dot"></div>
+        </div>
+    )
+};
+export default LoadingDots;
